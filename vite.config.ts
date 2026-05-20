93a6bce5597d837018c2d805808a88fa1f0c1797
Анна Кузнецова <annakuznecova@MacBook-Air-Anna.local>
2026-05-20 19:00:10 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/annakuzn.com/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
